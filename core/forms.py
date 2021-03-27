d08f486fbb4020a639f4f69d815b8adeaf3c185d
Akshay pk <65409941+prjctakshay@users.noreply.github.com>
2021-03-27 11:15:27 +0530

    updated forms

--- core/forms.py
+++ core/forms.py
@@ -1,8 +1,8 @@
 from django import forms
-
 from .models import ContactMe
 class ContactMeForm(forms.ModelForm):
 
     class Meta:
         model = ContactMe
+        #specify reqired feilds         
         fields = ('name', 'mail','phone_num','message')
